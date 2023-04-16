8abad47d4a74376d2f8fcb005911e577956afd36
SaifAlqady <siafalqady51@gmail.com>
2023-04-16 18:19:08 +0200

    Dropdown menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import cn from "@/lib/utils";
 import Providers from "@/components/Providers";
 import Navbar from "@/components/Navbar";
-
+import { Toaster } from "@/ui/Toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -19,6 +19,8 @@ export default function RootLayout({
       <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
         <Providers>
           {children}
+          <Toaster position="bottom-right"/>
+
           {/* @ts-expect-error Server Compoenet */}
           <Navbar />
         </Providers>

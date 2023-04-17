2d2757f6efc6183bfdf4a78c0a66f2e84a54b0cf
SaifAlqady <siafalqady51@gmail.com>
2023-04-18 01:38:19 +0200

    add Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import Paragraph from "../components/ui/Paragraph";
 import type { Metadata } from "next";
 import LargeHeading from "@/ui/LargeHeading";
+import Link from "next/link";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +20,14 @@ export default function Home() {
           <LargeHeading size='lg' className="three-d text-black dark:text-light-gold">
             Easily determine <br /> text similarity
           </LargeHeading>
+          <Paragraph className="max-w-xl lg:text-left">
+            With the text similarity API, you can easily determine the similarity between two pieces of text with a free{' '}
+            <Link href="/login" className="underline underline-offset-2 text-black dark:text-light-gold">API key</Link>
+            .
+          </Paragraph>
+          <div className="relative w-full max-w-lg lg:max-w-3xl lg:left-1/2 aspect-square lg:absolute">
+            <Image priority className="img-shadow" quality={100} style={{objectFit:'contain'}} fill src='/typewriter.png' alt="typewriter" />
+          </div>
         </div>
       </div>
     </div>

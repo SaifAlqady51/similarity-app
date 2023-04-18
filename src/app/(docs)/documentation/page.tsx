1035bfa5d481e1e5f176ebd4200d1e44999a0782
SaifAlqady <siafalqady51@gmail.com>
2023-04-18 02:12:55 +0200

    Creating documentaion page

---
+++ src/app/(docs)/documentation/page.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react'
+import type { Metadata } from 'next'
+import LargeHeading from '@/ui/LargeHeading'
+import Paragraph from '@/ui/Paragraph'
+
+export const metadata: Metadata = {
+    title: 'Similarity API | Documentation',
+    description: 'Free & open-source text similarity API'
+}
+
+const page: FC = ({}) => {
+  return <div className='container max-w-7xl mx-auto mt-12'>
+    <div className='flex flex-col items-center gap-6'>
+        <LargeHeading>
+            Making a request
+        </LargeHeading>
+        <Paragraph> api/v1/similarity</Paragraph>
+    </div>
+  </div>
+}
+
+export default page

64ca5e1c39f62e02413b71f829c15c266fb226d2
SaifAlqady <siafalqady51@gmail.com>
2023-04-27 03:37:37 +0200

    prisma database

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,52 @@
 import { NextAuthOptions } from "next-auth";
 import { db } from "@/lib/db";
 import {PrismaAdapter} from '@next-auth/prisma-adapter'
+import GoogleProvider from 'next-auth/providers/google'
+
+
+function getGoogleCredentials(): { clientId: string; clientSecret: string; }{
+    const clientId = process.env.GOOGLE_CLIENT_ID
+    const clientSecret = process.env.GOOGLE_CLIENT_SECRET
+
+    if(!clientId || clientId?.length === 0){
+        throw new Error('no clientId for google provider set')
+    }
+
+    if(!clientSecret || clientSecret?.length === 0){
+        throw new Error('no clientSecret for google provider set')
+    }
+
+    return {clientId,clientSecret}
+}
 
 export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(db)
+    adapter: PrismaAdapter(db),
+    session:{
+        strategy:'jwt'
+    },
+    pages:{
+        signIn:'/login'
+    },
+    providers: [
+        GoogleProvider({
+            clientId:getGoogleCredentials().clientId,
+            clientSecret:getGoogleCredentials().clientSecret,
+        })
+    ],
+    callbacks:{
+        async session({ session, token }) {
+
+            if (token && session.user) {
+            //   session.user.id = token.id
+              session.user.name = token.name
+              session.user.email = token.email
+              session.user.image = token.picture
+            }
+            return session
+          },
+
+        // async jwt({token,user}){
+        //     const dbUser = await db
+        // }
+    }
 }

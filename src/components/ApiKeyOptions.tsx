073a6d132f0dce18666661d49a4f64ae8b03e20f
SaifAlqady <siafalqady51@gmail.com>
2023-05-09 16:06:03 +0300

    change variables

--- src/components/ApiKeyOptions.tsx
+++ src/components/ApiKeyOptions.tsx
@@ -2,32 +2,52 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigge
 import { FC, useState } from 'react'
 import { Button } from './ui/Button'
 import { Loader2 } from 'lucide-react'
-
+import { toast } from './ui/Toast'
 interface ApiKeyOptionsProps {
-  
+  apiKeyId:string,
+  apiKeyText:string
 }
 
-const ApiKeyOptions: FC<ApiKeyOptionsProps> = ({}) => {
+const ApiKeyOptions: FC<ApiKeyOptionsProps> = ({apiKeyId, apiKeyText}) => {
     const [isCreatingNew, setIsCreatingNew] = useState<boolean>(false)
     const [isRevoking, setIsRevoking] = useState<boolean>(false)
 
-  return <DropdownMenu>
-    <DropdownMenuTrigger disabled={isCreatingNew || isRevoking} asChild>
-        <Button variant='ghost' className='flex gap-2 items-center'>
-            <p>
-                {isCreatingNew? 'Creating new Key': isRevoking? 'Revoking key': 'Options'}
-            </p>
-            {
-                isCreatingNew || isRevoking? (
-                    <Loader2 className='animate-spin h-4 w-4' />
-                ) : null
-            }
-        </Button>
-    </DropdownMenuTrigger>
-    <DropdownMenuContent>
-        <DropdownMenuItem></DropdownMenuItem>
-    </DropdownMenuContent>
-  </DropdownMenu>
+  return( 
+    <div>
+        <DropdownMenu>
+        <DropdownMenuTrigger disabled={isCreatingNew || isRevoking} asChild>
+            <Button variant='ghost' className='flex gap-2 items-center'>
+                <p>
+                    {isCreatingNew? 'Creating new Key': isRevoking? 'Revoking key': 'Options'}
+                </p>
+                {
+                    isCreatingNew || isRevoking? (
+                        <Loader2 className='animate-spin h-4 w-4' />
+                    ) : null
+                }
+            </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+            <DropdownMenuItem onClick={() => {
+                navigator.clipboard.writeText(apiKeyText)
+                toast({
+                    title:'Copied',
+                    message:'API key copied to clipboard',
+                    type:'success'
+                })
+            }}>
+
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                Create new key
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                Revoke new key
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
+  </div> 
+  )
 }
 
 export default ApiKeyOptions
